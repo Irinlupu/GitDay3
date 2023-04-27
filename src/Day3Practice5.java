f00bfbb4cfd2816ed38aa75b62a3b1d89b4bf2fd
Irina L <123971575+Irinlupu@users.noreply.github.com>
2023-04-27 11:31:56 -0400

    Created day 3 practice 5 file

---
+++ src/Day3Practice5.java
@@ -0,0 +1,7 @@
+public class Day3Practice5 {
+
+    // I enjoy git
+    // branches are useful
+    // get helps us collaborate with our teams efficiently
+    // git has layers that protect the application
+}
